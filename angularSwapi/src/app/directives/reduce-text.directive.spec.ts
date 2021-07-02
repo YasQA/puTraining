60b5f91f814b72c1c594eeee1ac6c471bd14cb00
Yaroslav Lebid <yaroslav.lebid@nixsolutions.com>
2021-07-02 14:07:25 +0300

    [angularSwapi_12] added directive for show more/less for long text

---
+++ angularSwapi/src/app/directives/reduce-text.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ToggleTextDirective } from './toggle-text.directive';
+
+describe('ReduceTextDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ToggleTextDirective();
+    expect(directive).toBeTruthy();
+  });
+});
